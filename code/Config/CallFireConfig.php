d31cbc51f8fa72d6dc7ccc5f99005b94e82af887
helpfulrobot <chris@silverstripe.com>
2016-01-01 05:06:39 +1300

    Converted to PSR-2

--- code/Config/CallFireConfig.php
+++ code/Config/CallFireConfig.php
@@ -1,18 +1,21 @@
 <?php
-class CallFireConfig extends DataExtension {
-	private static $db = array(
-		"CallFire_ApiUsername" => "Varchar",
-		"CallFire_ApiPassword" => "Varchar"
-	);
-	
-	public function updateCMSFields(FieldList $fields) {
-		$fields->addFieldsToTab("Root.CallFire", array(
-			TextField::create("CallFire_ApiUsername", "API Username"),
-			TextField::create("CallFire_ApiPassword", "API Password")
-		));
-	}
-	
-	public function getCallFireClient($apis) {
-		return CallFire\Api\Client::Rest($this->owner->CallFire_ApiUsername, $this->owner->CallFire_ApiPassword, $apis);
-	}
+class CallFireConfig extends DataExtension
+{
+    private static $db = array(
+        "CallFire_ApiUsername" => "Varchar",
+        "CallFire_ApiPassword" => "Varchar"
+    );
+    
+    public function updateCMSFields(FieldList $fields)
+    {
+        $fields->addFieldsToTab("Root.CallFire", array(
+            TextField::create("CallFire_ApiUsername", "API Username"),
+            TextField::create("CallFire_ApiPassword", "API Password")
+        ));
+    }
+    
+    public function getCallFireClient($apis)
+    {
+        return CallFire\Api\Client::Rest($this->owner->CallFire_ApiUsername, $this->owner->CallFire_ApiPassword, $apis);
+    }
 }
